8357772fde3a57fc3adbcedda2040e528c00224e
Md Mahamodul Islam <mahamodul.shakil11@gmail.com>
2024-04-03 17:05:35 +0200

    added basic useage of httpget

--- MagicVillaAPI/Controllers/VillaAPIController.cs
+++ MagicVillaAPI/Controllers/VillaAPIController.cs
@@ -1,21 +1,30 @@
-﻿using MagicVillaAPI.Models;
+﻿using MagicVillaAPI.Data;
+using MagicVillaAPI.Models;
+using MagicVillaAPI.Models.DTO;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVillaAPI.Controllers
 {
-    [Route("api/[controller]")]
+    //[Route("api/[controller]")]
+    [Route("api/VillaAPI")]
+
     [ApiController]
     public class VillaAPIController : ControllerBase
     {
 
         [HttpGet]
-        public IEnumerable<Villa> GetVillas()
+        public IEnumerable<VillaDTO> GetVillas()
         {
-            return new List<Villa> {
-            new Villa{Id=1,Name="Pool View" },
-            new Villa{Id=2,Name="Beach View" }
-            };
+            return VillaStore.villaList;
+
+        }
+
+
+        [HttpGet("{id:int}")]
+        public VillaDTO GetVilla(int id)
+        {
+            return VillaStore.villaList.FirstOrDefault(u => u.Id == id);
         }
     }
 }
