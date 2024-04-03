4db87bcf00c94d6c2797af35052c9beae476c6a3
Md Mahamodul Islam <mahamodul.shakil11@gmail.com>
2024-04-03 16:33:52 +0200

    added basic api test and cleanup

---
+++ MagicVillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,21 @@
+﻿using MagicVillaAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVillaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa> {
+            new Villa{Id=1,Name="Pool View" },
+            new Villa{Id=2,Name="Beach View" }
+            };
+        }
+    }
+}
