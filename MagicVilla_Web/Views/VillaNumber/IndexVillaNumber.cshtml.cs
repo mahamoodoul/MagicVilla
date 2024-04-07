a37c97f9581c009642a03ff08b8d072eca4aa8a5
Md Mahamodul Islam <mahamodul.shakil11@gmail.com>
2024-04-07 18:37:36 +0200

    getting all villas

---
+++ MagicVilla_Web/Views/VillaNumber/IndexVillaNumber.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MagicVilla_Web.Views.VillaNumber;
+
+public class IndexVillaNumber : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
