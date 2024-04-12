8ff35008528c7f24348f7a440caf93e9479374cb
Md Mahamodul Islam <mahamodul.shakil11@gmail.com>
2024-04-12 22:56:08 +0200

    added user controller for auth user

--- MagicVillaAPI/Repository/UserRepository.cs
+++ MagicVillaAPI/Repository/UserRepository.cs
@@ -37,7 +37,11 @@ namespace MagicVillaAPI.Repository
 
             if (user == null)
             {
-                return null;
+                return new LoginResponseDTO()
+                {
+                    Token = "",
+                    User = null
+                };
             }
 
             //if user was found generate JWT Token
