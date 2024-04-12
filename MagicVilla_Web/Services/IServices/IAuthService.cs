b49ab78e22afe306a5ea8f26e1c8483c26bd513f
Md Mahamodul Islam <mahamodul.shakil11@gmail.com>
2024-04-13 00:08:07 +0200

    add auth service

---
+++ MagicVilla_Web/Services/IServices/IAuthService.cs
@@ -0,0 +1,10 @@
+using MagicVilla_Web.Models.DTO;
+
+namespace MagicVilla_Web.Services.IServices
+{
+    public interface IAuthService
+    {
+        Task<T> LoginAsync<T>(LoginRequestDTO objToCreate);
+        Task<T> RegisterAsync<T>(RegisterationRequestDTO objToCreate);
+    }
+}
